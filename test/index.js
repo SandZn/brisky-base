1a0985cb46faab1be2cf5554cc1c73c141f8bfd1
Jim de Beer <jim@vigour.io>
2016-02-24 09:31:05 +0100

    added tape tests

---
+++ test/index.js
@@ -0,0 +1,22 @@
+'use strict'
+var test = require('tape')
+var isEmpty = require('vigour-util/is/empty')
+var isRemoved = require('vigour-util/is/removed')
+var Base = require('../')
+
+test('keys', function (t) {
+  var base = new Base({
+    a: true,
+    b: true
+  })
+  t.plan(5)
+  t.equal(base.keys().length, 2)
+  base.removeProperty(base.a, 'a')
+  t.equal(base.keys().length, 1)
+  base.setKey('c', true)
+  t.equal(base.keys().length, 2)
+  base.clear()
+  t.equal(base.keys(), false)
+  base.set({ d: true })
+  t.equal(base.keys().length, 1)
+})
