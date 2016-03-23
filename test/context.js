45cc5ec0b5f62d122a7c61c784d0f01ade2ea5cc
Jim de Beer <jim@vigour.io>
2016-03-23 19:50:50 +0100

    constructor / cntxkey tests

--- test/context.js
+++ test/context.js
@@ -21,3 +21,13 @@ test('context override (noContext property)', function (t) {
     'setting noContextField does not create a new instance'
   )
 })
+
+test('context keys', function (t) {
+  t.plan(3)
+  var a = new Base({ a: true })
+  t.equal(a._a, void 0, 'no context key before creation of constructor')
+  var A = a.Constructor
+  t.equal(a._a, a.a, 'has context key after creation of constructor')
+  var b = new A({ b: true })
+  t.equal(b._b, void 0, 'no context key before creation of inhertied constructor')
+})
