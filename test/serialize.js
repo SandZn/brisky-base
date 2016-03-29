908411dec18539a41ef8c9564ba1b2cd41822665
Jim de Beer <jim@vigour.io>
2016-03-29 22:51:21 +0200

    serialize computed (replacement of plain)

--- test/serialize.js
+++ test/serialize.js
@@ -5,23 +5,29 @@ const Base = require('../')
 test('serialize', function (t) {
   const cases = [
     { a: true, b: { c: 'yo' } },
-    { a: true, b: { val: [ '$', 'a' ] } }
+    [
+      { a: true, b: { val: ['$', 'a'] } },
+      { a: true, b: ['$', 'a'] }
+    ]
   ]
   t.plan(cases.length)
   cases.forEach(function (item) {
-    const base = new Base(item) // this is pretty bad get rid of the reuse
-    t.deepEquals(base.serialize(), item, base)
+    const base = new Base(item[0] || item)
+    t.deepEquals(base.serialize(), item[1] || item, base)
   })
 })
 
 test('serialize-computed', function (t) {
   const cases = [
     { a: true, b: { c: 'yo' } },
-    { a: true, b: { val: [ '$', 'a' ] } }
+    [
+      { a: 'a', b: { val: [ '$', 'a' ] } },
+      { a: 'a', b: 'a' }
+    ]
   ]
   t.plan(cases.length)
   cases.forEach(function (item) {
-    const base = new Base(item) // this is pretty bad get rid of the reuse
-    t.deepEquals(base.serialize(), item, base)
+    const base = new Base(item[0] || item)
+    t.deepEquals(base.serialize(true), item[1] || item, base)
   })
 })
