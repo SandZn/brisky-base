0996da1feb9ac761553b2fd309d790b0cce0c7b7
Jim de Beer <jim@vigour.io>
2016-03-30 21:55:33 +0200

    useVal

--- test/useval.js
+++ test/useval.js
@@ -3,12 +3,21 @@ const test = require('tape')
 const Base = require('../')
 
 test('useval', function (t) {
-  const a = new Base({ val: 'a', useVal: true })
+  const a = new Base({
+    val: 'a',
+    useVal: true,
+    b: { val: 'b', useVal: true }
+  })
   const b = new a.Constructor()
   const c = new Base({ b: b })
-  t.plan(2)
+  t.plan(4)
   t.equal(c.b, b, 'inherited useVal')
   const specialObj = { hello: true }
   c.set({ x: { useVal: specialObj } })
   t.equal(c.x, specialObj, 'setValue using useVal')
+  const d = new Base(c.b)
+  t.equal(d.val, c.b, 'make a reference to a useVal base')
+  const abInstance = new a.b.Constructor()
+  const e = new Base({ key: 'e', b: abInstance })
+  t.equal(e.b, abInstance, 'make a reference to a useVal base nested instance')
 })
