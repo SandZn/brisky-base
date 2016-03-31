eba0941b968e569220a1149c081c68e29ae04f9d
Jim de Beer <jim@vigour.io>
2016-03-31 09:56:28 +0200

    rename useVal to noReference remove the standard behaviour for useVal: {} (to overide Child)

---
+++ test/noreference.js
@@ -0,0 +1,20 @@
+'use strict'
+const test = require('tape')
+const Base = require('../')
+
+test('noReference', function (t) {
+  const a = new Base({
+    val: 'a',
+    noReference: true,
+    b: { val: 'b', noReference: true }
+  })
+  const b = new a.Constructor()
+  const c = new Base({ b: b })
+  const d = new Base(c.b)
+  const abInstance = new a.b.Constructor()
+  const e = new Base({ key: 'e', b: abInstance })
+  t.plan(4)
+  t.equal(c.b, b, 'inherited noReference')
+  t.equal(d.val, c.b, 'make a reference to a noReference base when it has its own parent (no move)')
+  t.equal(e.b, abInstance, 'do not make a reference to a noReference base nested instance')
+})
