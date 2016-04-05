21f32160679264d07dff373e4aa530da150e754a
Jim de Beer <jim@vigour.io>
2016-04-05 12:32:40 +0200

    added contextCandidate tests, refactor context getter creator

--- test/context.js
+++ test/context.js
@@ -24,22 +24,45 @@ test('context override (noContext property)', function (t) {
 
 test('context keys', function (t) {
   t.plan(3)
-  var a = new Base({ a: true })
+  const a = new Base({ a: true })
   t.equal(a._a, void 0, 'no context key before creation of constructor')
-  var A = a.Constructor
+  const A = a.Constructor
   t.equal(a._a, a.a, 'has context key after creation of constructor')
-  var b = new A({ b: true })
+  const b = new A({ b: true })
   t.equal(b._b, void 0, 'no context key before creation of inhertied constructor')
 })
 
 test('context resolvement', function (t) {
   t.plan(3)
-  var base = new Base({ a: { b: 'b' } })
-  var instance = new base.Constructor()
+  const base = new Base({ a: { b: 'b' } })
+  const instance = new base.Constructor()
   instance.a.b.set('hello')
   t.equal(instance._a._b !== base._a._b, true, 'resolved context')
-  var instance2 = new base.Constructor()
+  const instance2 = new base.Constructor()
   instance2.a.b.remove()
   t.equal(instance2._a._b, null, 'removed b from instance2')
   t.equal(instance2._a !== base._a, true, 'resolved context for a')
+})
+
+test('nested context resolvement for override properties', function (t) {
+  t.plan(6)
+  const On = new Base().Constructor
+  const base = new Base({
+    properties: {
+      on: { val: On, override: '_on' }
+    },
+    on: {},
+    normal: {}
+  })
+  const a = new base.Constructor({
+    on: { data: 'hello' },
+    normal: { data: 'normal-hello' }
+  })
+  t.equal('data' in a._on, true, 'has _on.data')
+  t.equal('data' in a.normal, true, 'has normal.data')
+  t.equal('_data' in a._on, false, 'does not have _on._data')
+  t.equal('_data' in a.normal, false, 'does not have normal._data')
+  a.Constructor
+  t.equal('_data' in a._on, true, 'has _on._data after constructor creation')
+  t.equal('_data' in a.normal, true, 'has normal._data after constructor creation')
 })
