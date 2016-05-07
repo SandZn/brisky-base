5ff5a2495867722fdbaa7bdf954dfaaa86dab15e
Jim de Beer <jim@vigour.io>
2016-05-07 15:13:29 +0200

    added fix for keys when a field is in the state of being removed

--- test/keys.js
+++ test/keys.js
@@ -9,7 +9,7 @@ test('keys', function (t) {
     b: true,
     val: 'something'
   })
-  t.plan(7)
+  t.plan(8)
   t.equal(base.keys().length, 2, 'correct length')
   base.removeProperty(base.a, 'a')
   t.equal(base.keys().length, 1, 'correct length after removal')
@@ -22,6 +22,9 @@ test('keys', function (t) {
   t.equal(base.keys().length, 1, 'correct length after set')
   base.set({ c: null })
   t.equal(base.keys().length, 1, 'correct length after set with null')
+  base.set({ e: true })
+  base.e.val = null
+  t.equal(base.keys().length, 1, 'correct length after set when field is being removed')
 })
 
 test('ordered keys', function (t) {
