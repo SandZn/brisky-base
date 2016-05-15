668288e6a6da61585678b3d7f8db5c32e0c0bb44
Jim de Beer <jim@vigour.io>
2016-05-15 19:29:10 +0200

    accedently remove dset tests

--- test/set.js
+++ test/set.js
@@ -2,7 +2,7 @@
 const test = require('tape')
 const Base = require('../')
 
-test('lookup', function (t) {
+test('set - resolve context', function (t) {
   t.plan(2)
   var a = new Base({
     b: {
@@ -13,4 +13,36 @@ test('lookup', function (t) {
   t.same(b.b.c.path(), [ 'b', 'b', 'c'], 'correct resolved contextPath')
   b.b.set({ something: true })
   t.equal(b.hasOwnProperty('_b'), true, 'resolved b.b after set')
+})
+
+test('set - type', function (t) {
+  t.plan(2)
+  var a = new Base({
+    components: {
+      x: 'lulllz'
+    },
+    x: 'something'
+  })
+  var x = a.x.uid()
+  a.set({ x: { type: 'x' } })
+  t.equal(a.x.val, 'lulllz', 'created new x type')
+  t.equal(x !== a.x.uid(), true, 'overwritten old x property')
+  t.end()
+})
+
+test('set - reserved key error', function (t) {
+  t.plan(1)
+  var a = new Base({
+    define: {
+      field: { value: 'haha reserved!' }
+    }
+  })
+  try {
+    a.set({
+      field: 'haha'
+    })
+  } catch(e) {
+    t.equal(e.message, 'cannot set property "field", on "base" ')
+  }
+  t.end()
 })
