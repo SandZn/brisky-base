67efd13a556fd5d1838b175b258591fec976fb8b
Jim de Beer <jim@vigour.io>
2016-05-15 17:55:06 +0200

    added more property tests

--- test/property/basic.js
+++ test/property/basic.js
@@ -1,8 +1,8 @@
 'use strict'
 const test = require('tape')
-const Base = require('../')
+const Base = require('../../')
 
-test('set property', function (t) {
+test('property - basic - set', function (t) {
   t.plan(2)
   var cnt = 0
   const base = new Base({ set () { cnt++ } })
@@ -12,7 +12,7 @@ test('set property', function (t) {
   t.equal(cnt, 1, 'set hook fired once')
 })
 
-test('origin property', function (t) {
+test('property - basic - origin', function (t) {
   t.plan(1)
   const a = new Base({})
   const b = new Base(a)
@@ -20,7 +20,7 @@ test('origin property', function (t) {
   t.equal(a.val, 1, 'a.val equals 1')
 })
 
-test('set val property to null', function (t) {
+test('property - basic - set val definition to null', function (t) {
   t.plan(2)
   const a = new Base({
     properties: { val: null },
@@ -30,4 +30,19 @@ test('set val property to null', function (t) {
   b.set({ c: void 0, val: 1 })
   t.equal(b.val instanceof Base, true, 'val is a normal base')
   t.equal(b.c instanceof Base, true, 'field c gets created')
+})
+
+test('property - basic - wrong property type error', function (t) {
+  t.plan(1)
+  try {
+    const a = new Base({
+      properties: { val: void 0 },
+      Child: 'Constructor'
+    })
+  } catch (e) {
+    t.equal(
+      e.message,
+      'base.properties - uncaught property type val: "undefined"'
+    )
+  }
 })
