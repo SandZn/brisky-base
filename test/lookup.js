31c70ed8df07c978936525c6feca2022f8796f58
Jim de Beer <jim@vigour.io>
2016-05-15 19:27:55 +0200

    added lookup tests

---
+++ test/lookup.js
@@ -0,0 +1,33 @@
+'use strict'
+const test = require('tape')
+const Base = require('../')
+
+test('lookUp', function (t) {
+  const a = new Base({
+    b: {
+      c: {
+        d: true
+      },
+      e: true
+    },
+    e: 'hello',
+    something: true
+  })
+  t.equal(a.something.lookUp('b.c.d'), a.b.c.d, 'gets parent.b.c.d')
+  t.equal(
+    a.something.lookUp([ 'b', 'c', 'd' ]),
+    a.b.c.d,
+    'gets parent.b.c.d using array notation'
+  )
+  t.equal(a.b.c.d.lookUp('something'), a.something, 'gets something')
+  t.equal(a.b.c.d.lookUp('e'), a.b.e, 'gets e')
+  t.equal(a.b.c.d.lookUp('e', function (result) {
+    if(result.val === 'hello') {
+      return result
+    }
+  }), a.e, 'gets top e using filter')
+  t.equal(a.b.c.d.lookUp('e', /^hell/), a.e, 'gets top e using regExp')
+  t.equal(a.b.c.d.lookUp('e', 'hello'), a.e, 'gets top e using primitive')
+  t.equal(a.b.c.d.lookUp('e', [ 'hello', 'flabber' ]), a.e, 'gets top e using array')
+  t.end()
+})
--- test/set.js
+++ test/set.js
@@ -2,7 +2,7 @@
 const test = require('tape')
 const Base = require('../')
 
-test('set - resolve context', function (t) {
+test('lookup', function (t) {
   t.plan(2)
   var a = new Base({
     b: {
@@ -13,36 +13,4 @@ test('set - resolve context', function (t) {
   t.same(b.b.c.path(), [ 'b', 'b', 'c'], 'correct resolved contextPath')
   b.b.set({ something: true })
   t.equal(b.hasOwnProperty('_b'), true, 'resolved b.b after set')
-})
-
-test('set - type', function (t) {
-  t.plan(2)
-  var a = new Base({
-    components: {
-      x: 'lulllz'
-    },
-    x: 'something'
-  })
-  var x = a.x.uid()
-  a.set({ x: { type: 'x' } })
-  t.equal(a.x.val, 'lulllz', 'created new x type')
-  t.equal(x !== a.x.uid(), true, 'overwritten old x property')
-  t.end()
-})
-
-test('set - reserved key error', function (t) {
-  t.plan(1)
-  var a = new Base({
-    define: {
-      field: { value: 'haha reserved!' }
-    }
-  })
-  try {
-    a.set({
-      field: 'haha'
-    })
-  } catch(e) {
-    t.equal(e.message, 'cannot set property "field", on "base" ')
-  }
-  t.end()
 })
