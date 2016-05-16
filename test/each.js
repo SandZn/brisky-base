5ed817a3883042d70dcda4359123a53d260208b5
Jim de Beer <jim@vigour.io>
2016-05-16 10:20:06 +0200

    added tests for each

---
+++ test/each.js
@@ -0,0 +1,53 @@
+'use strict'
+const test = require('tape')
+const Base = require('../')
+
+test('each', function (t) {
+  const a = new Base({
+    a: 'a',
+    b: 'b',
+    c: 'c',
+    d: {
+      keyType: 'special'
+    },
+    e: 'e'
+  })
+  var results = {}
+  a.each((p, key) => {
+    results[key] = results[key] ? results[key]++ : 1
+  })
+  t.same(
+    results,
+    { a: 1, b: 1, c: 1, e: 1 },
+    'each loops trough all normal keys, excludes d'
+  )
+
+  results = {}
+  a.each((p, key) => {
+    results[key] = results[key] ? results[key] + 1 : 1
+    if (key === 'd') {
+      return true
+    }
+  }, (p, key) => p && p._base_version)
+  t.same(
+    results,
+    { a: 1, b: 1, c: 1, d: 1 },
+    'filtered each loops trough all keys, return breaks loop'
+  )
+
+  results = {}
+  a.each((p, key) => {
+    results[key] = results[key] ? results[key]++ : 1
+    if (key === 'b') {
+      // this breaks the loop
+      return true
+    }
+  })
+  t.same(
+    results,
+    { a: 1, b: 1 },
+    'each loops trough all normal keys, return breaks loop'
+  )
+
+  t.end()
+})
