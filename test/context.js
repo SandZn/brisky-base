5aef8cf1fa63a7080337861dae23198251c7a9e7
Jim de Beer <jim@vigour.io>
2016-05-16 12:42:56 +0200

    added repair context tests

--- test/context.js
+++ test/context.js
@@ -2,8 +2,7 @@
 const test = require('tape')
 const Base = require('../')
 
-test('context override (noContext property)', function (t) {
-  t.plan(2)
+test('context - override (noContext property)', function (t) {
   const Template = new Base({
     key: 'template',
     noContextField: { noContext: true }
@@ -20,20 +19,44 @@ test('context override (noContext property)', function (t) {
     'hello',
     'setting noContextField does not create a new instance'
   )
+  t.end()
 })
 
-test('context keys', function (t) {
-  t.plan(3)
+test('context - parent', function (t) {
+  const base = new Base({
+    a: {
+      b: {
+        c: {
+          d: 'd'
+        }
+      }
+    }
+  })
+  const instance = new base.Constructor({
+    a: {
+      b: 'my own!'
+    }
+  })
+  t.equal(instance.a.b.c.d.cParent(), base.a.b.c, 'normal parent')
+  t.equal(instance.a.b.c.cParent(), instance.a.b, 'context')
+  const a = instance.a
+  const b = instance.a.b
+  a.__c = base
+  t.equal(b.cParent(), a, 'repairs illegal context')
+  t.end()
+})
+
+test('context - keys', function (t) {
   const a = new Base({ a: true })
   t.equal(a._a, void 0, 'no context key before creation of constructor')
   const A = a.Constructor
   t.equal(a._a, a.a, 'has context key after creation of constructor')
   const b = new A({ b: true })
   t.equal(b._b, void 0, 'no context key before creation of inhertied constructor')
+  t.end()
 })
 
-test('context resolvement', function (t) {
-  t.plan(3)
+test('context - resolvement', function (t) {
   const base = new Base({ a: { b: 'b' } })
   const instance = new base.Constructor()
   instance.a.b.set('hello')
@@ -42,10 +65,10 @@ test('context resolvement', function (t) {
   instance2.a.b.remove()
   t.equal(instance2._a._b, null, 'removed b from instance2')
   t.equal(instance2._a !== base._a, true, 'resolved context for a')
+  t.end()
 })
 
-test('nested context resolvement for override properties', function (t) {
-  t.plan(7)
+test('context - nested resolvement for override properties', function (t) {
   const On = new Base().Constructor
   const ref = new Base()
   const base = new Base({
@@ -67,16 +90,5 @@ test('nested context resolvement for override properties', function (t) {
   a.Constructor
   t.equal('_data' in a._on, true, 'has _on._data after constructor creation')
   t.equal('_data' in a.normal, true, 'has normal._data after constructor creation')
-})
-
-test('context store', function (t) {
-  t.plan(3)
-  const base = new Base({ a: { b: 'b' } })
-  const instance = new base.Constructor()
-  instance.a.b.set('hello')
-  t.equal(instance._a._b !== base._a._b, true, 'resolved context')
-  const instance2 = new base.Constructor()
-  instance2.a.b.remove()
-  t.equal(instance2._a._b, null, 'removed b from instance2')
-  t.equal(instance2._a !== base._a, true, 'resolved context for a')
+  t.end()
 })
