7d27c8b88a11781cd6ecd2a32874b3c2f03303db
Jim de Beer <jim@vigour.io>
2016-05-22 20:12:11 +0200

    split off child, added child tests

--- test/child.js
+++ test/child.js
@@ -1,25 +1,26 @@
-// 'use strict'
+'use strict'
+var test = require('tape')
+var Base = require('../')
 
-// test('child - inheritance', function (t) {
-//   const a = new Base({
-//     types: {
-//       special: 'hello',
-//       something: {
-//         type: 'special'
-//       }
-//     }
-//   })
-//   a.set({
-//     field: {
-//       type: 'something',
-//       field: 'hello'
-//     }
-//   })
-//   t.same(a.serialize(), {
-//     field: {
-//       field: 'hello',
-//       val: 'hello'
-//     }
-//   }, 'something inherits from special')
-//   t.end()
-// })
+test('child', function (t) {
+  const bChild = new Base('bye')
+  function Special (val) {
+    this.internalStuff = val
+  }
+
+  const a = new Base({
+    Child: { val: 'hello' },
+    b: { Child: bChild },
+    c: { Child: Special }
+  })
+
+  t.same(a.b.serialize(), 'hello', 'object notation')
+  a.b.set({ c: {} })
+  t.same(a.b.c.serialize(), 'bye', 'base notation')
+  a.c.set({ special: 'hello' })
+  t.same(a.c.special instanceof Special, true, 'custom constructor')
+  a.set({ Child: { field: true } })
+  t.same(a.b.field.serialize(), true, 'b - use set when child is allready defined')
+  t.same(a.c.field.serialize(), true, 'c - use set when child is allready defined')
+  t.end()
+})
