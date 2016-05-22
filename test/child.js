82d05c18f2848a050bfb03c6122c05e791927d77
Jim de Beer <jim@vigour.io>
2016-05-22 18:51:25 +0200

    added funcitonality to types to be able to add non object values, added tests

---
+++ test/child.js
@@ -0,0 +1,25 @@
+// 'use strict'
+
+// test('child - inheritance', function (t) {
+//   const a = new Base({
+//     types: {
+//       special: 'hello',
+//       something: {
+//         type: 'special'
+//       }
+//     }
+//   })
+//   a.set({
+//     field: {
+//       type: 'something',
+//       field: 'hello'
+//     }
+//   })
+//   t.same(a.serialize(), {
+//     field: {
+//       field: 'hello',
+//       val: 'hello'
+//     }
+//   }, 'something inherits from special')
+//   t.end()
+// })
--- test/types.js
+++ test/types.js
@@ -2,12 +2,11 @@
 var test = require('tape')
 var Base = require('../')
 
-test('type override', function (t) {
-  t.plan(4)
-  var Template = new Base({
+test('types - remove type property', function (t) {
+  const Template = new Base({
     type: 'template'
   }).Constructor
-  var TemplateA = new Template({
+  const TemplateA = new Template({
     key: 'TemplateA',
     properties: { type: null },
     Child: {
@@ -16,23 +15,107 @@ test('type override', function (t) {
     },
     type: 'this is something'
   }).Constructor
-  var a = new TemplateA()
+  const a = new TemplateA()
   t.equals(
     a.type.type,
     'something',
     'a.type is an instance of Child with type something'
   )
   t.equals(a.type.val, 'this is something', 'a.type has a correct input value')
-  var TemplateB = new Template({
+  const TemplateB = new Template({
     properties: {
       type: new Base({ type: 'special' })
     }
   }).Constructor
-  var b = new TemplateB({ type: 'this is special' })
+  const b = new TemplateB({ type: 'this is special' })
   t.equals(
     b.type.val,
     'this is special',
     'b.type has a correct input value'
   )
   t.equals(b.type.type, 'special', 'b.type has type "special"')
+  t.end()
+})
+
+test('types - create types', function (t) {
+  const c = new Base({ special: true })
+  const d = new Base({ specialD: true })
+  const a = new Base({
+    types: {
+      b: {
+        field: true,
+        extra: true
+      },
+      c: c,
+      d: d
+    }
+  })
+  const a2 = new a.Constructor({
+    types: {
+      b: {
+        field: false,
+        field2: true
+      }
+    }
+  })
+  t.same(a2.types, {
+    b: {
+      field: false,
+      field2: true,
+      extra: true
+    },
+    c: c,
+    d: d
+  }, 'merges object')
+  const a3 = new a.Constructor({
+    types: { d: { val: 'lulz' } }
+  })
+  t.same(
+    a3.types.d.serialize(),
+    {
+      val: 'lulz',
+      specialD: true
+    },
+    'injects for base types'
+  )
+  const a4 = new Base({
+    types: [ { a: true }, { b: true } ]
+  })
+  a4.set({
+    types: {
+      a: { bla: true }
+    }
+  })
+  t.same(a4.types, {
+    a: {
+      val: true,
+      bla: true
+    },
+    b: true
+  }, 'use array notation, merge to non-object')
+  t.end()
+})
+
+test('types - inheritance', function (t) {
+  const a = new Base({
+    types: {
+      special: 'hello',
+      something: {
+        type: 'special'
+      }
+    }
+  })
+  a.set({
+    field: {
+      type: 'something',
+      field: 'hello'
+    }
+  })
+  t.same(a.serialize(), {
+    field: {
+      field: 'hello',
+      val: 'hello'
+    }
+  }, 'something inherits from special')
+  t.end()
 })
