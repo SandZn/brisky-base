6d1db535e0efad6c4f9ea647ee803f65bac21af5
Jim de Beer <jim@vigour.io>
2016-05-25 20:17:26 +0200

    finished all edge cases with moving and nulling key field in define

--- test/property/define.js
+++ test/property/define.js
@@ -30,6 +30,10 @@ test('property - define - default - key', function (t) {
     }
   })
   t.same(base.x === 0 && base.z === null, true, 'moved property z → x')
+  base.set({
+    x: 'hello'
+  })
+  t.same(base.x, 'hello', 'redefined property x → x')
   t.end()
 })
 
@@ -50,23 +54,25 @@ test('property - define - base - key', function (t) {
       define: {
         x: { key: 'z' }
       }
-    }
+    },
+    x: 'its z!'
   })
-  t.same(base.z && base.z.val === 0 && base.y === null, true, 'moved property y → z')
+  t.same(base.z && base.z.val === 'its z!' && base.y === null, true, 'moved property y → z')
   t.same(base.properties.x.base.key, 'z', 'property got correct key')
   t.same(base.properties.keyMap, { x: 'z' }, 'has correct key map after move')
 
-  console.log('MOVE BITCH')
   base.set({
     properties: {
       define: {
         x: {
           key: null,
-          val: 'hello'
+          val: { field: 'hello' }
         }
       }
     }
   })
-  console.log('yo yo yo yo', base, base.properties)
+  t.same(base.x && base.x.isBase && base.z === null, true, 'moved property z → x')
+  t.same(base.x && base.x.field.val, 'hello', 'set property x')
+  t.same(base.properties.keyMap, null, 'remove property map')
   t.end()
 })
