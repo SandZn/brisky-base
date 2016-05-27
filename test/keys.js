688a91484eac9740f1148da21bd7a25568b97458
Jim de Beer <jim@vigour.io>
2016-05-27 19:22:25 +0200

    fixed keys inheritiance

--- test/keys.js
+++ test/keys.js
@@ -21,6 +21,39 @@ test('keys', function (t) {
   t.equal(base.keys().length, 1, 'correct length after set')
   base.set({ c: null })
   t.equal(base.keys().length, 1, 'correct length after set with null')
+  const instance = new base.Constructor({
+    x: true, y: true, z: true, special: null
+  })
+  t.same(
+    instance.keys(),
+    [ 'd', 'x', 'y', 'z' ],
+    'merge keys for instance'
+  )
+  base.set({ special: true, e: true, y: true })
+  t.same(
+    instance.keys(),
+    [ 'd', 'x', 'y', 'z', 'e' ],
+    'add "e", do not add "special" (nulled)'
+  )
+  base.d.remove()
+  base.y.remove()
+  t.same(
+    instance.keys(),
+    [ 'x', 'y', 'z', 'e' ],
+    'removed "d", do not remove "y"'
+  )
+  const instance2 = new instance.Constructor({ b: true })
+  t.same(
+    instance2.keys(),
+    [ 'x', 'y', 'z', 'e', 'b' ],
+    'instance2 has correct keys'
+  )
+  base.set({ h: true })
+  t.same(
+    instance2.keys(),
+    [ 'x', 'y', 'z', 'e', 'b', 'h' ],
+    'after updating base instance2 has correct keys'
+  )
   t.end()
 })
 
@@ -52,23 +85,75 @@ test('filtered keys', function (t) {
     something: { type: 'thing' }
   })
   t.same(base.keys(), [ 'etc' ], 'correct normal keys')
-  t.same(base.keys('thing'), [ 'something' ], 'correct "thing" keys')
+  t.same(
+    base.keys('thing'),
+    [ 'something' ],
+    'correct "thing" keys'
+  )
 
   base.set({ other: { type: 'thing' } })
-  t.same(base.keys('thing'), [ 'something', 'other' ], 'correct "thing" keys after add')
+  t.same(
+    base.keys('thing'),
+    [ 'something', 'other' ],
+    'correct "thing" keys after add'
+  )
   base.set({ something: null })
-  t.same(base.keys('thing'), [ 'other' ], 'correct "thing" keys after remove')
+  t.same(
+    base.keys('thing'),
+    [ 'other' ],
+    'correct "thing" keys after remove'
+  )
 
   const instance = new base.Constructor({
     bla: { type: 'thing' },
     field: true
   })
-  t.same(instance.keys('thing'), [ 'other', 'bla' ], 'correct "thing" keys on instance')
+  t.same(
+    instance.keys('thing'),
+    [ 'other', 'bla' ],
+    'correct "thing" keys on instance'
+  )
+  t.same(
+    base.keys('thing'),
+    [ 'other' ],
+    'original did not get polluted by instance'
+  )
+  console.log('yo wtf? instance does not call real remove...')
+  // instance.other.remove()
+  instance.set({ other: null })
+  t.same(instance.keys('thing'), [ 'bla' ], 'correct "thing" keys on instance after remove')
   t.same(base.keys('thing'), [ 'other' ], 'original did not get polluted by instance')
+  // instance.reset()
+  // t.same(instance.keys('thing'), [], 'reset keys')
+  // t.same(base.keys('thing'), [ 'other' ], 'original did not get polluted by instance')
+  t.end()
+})
 
-  console.log(base._i)
+// test('has correct keys inheritance', function (t) {
+//   var BaseExample = new Base({
+//     something: true,
+//     hello: '?',
+//     child: 'Constructor'
+//   }).Constructor
+
+//   var a = new BaseExample({
+//     b: {
+//       c: {
+//         something: { hello: {} }
+//       }
+//     }
+//   })
+//   t.same(a.something.keys(), [], 'a keys are false')
+//   t.same(a.b.something.keys(), [], 'a.b keys are false')
+//   t.same(a.b.c.something.keys(), [ 'hello' ], 'a.b.c keys equal [ "hello" ]')
+//   t.end()
+// })
+
+/*
+  console.log(base._instances())
 
   // edge case where the orginal does not have filtered keys and the later gets them -- no inheritance for the instance
+  console.log('INHERITANCE!')
   const a = new Base({
     types: {
       thing: {
@@ -102,37 +187,5 @@ test('filtered keys', function (t) {
   // best solution is to just clear everything in instances
   console.log(a.keys('thing'), b.keys('things'), c.keys())
   console.log(b.thing)
-  // need instance updates supe rimportant
-  // does this meanw e need to move the instances stuff to base
-  // probably yes
-  // yes move it to here
-  // console.log(a.keys(), b.keys())
-  // instance.set({ other: null })
-  // t.same(instance.keys('thing'), [ 'bla' ], 'correct "thing" keys on instance after remove')
-  // t.same(base.keys('thing'), [ 'other' ], 'original did not get polluted by instance')
-  // instance.reset()
-  // t.same(instance.keys('thing'), [], 'reset keys')
-  // t.same(base.keys('thing'), [ 'other' ], 'original did not get polluted by instance')
-  // now add creation etc
-  t.end()
-})
-
-// test('has correct keys inheritance', function (t) {
-//   var BaseExample = new Base({
-//     something: true,
-//     hello: '?',
-//     child: 'Constructor'
-//   }).Constructor
-
-//   var a = new BaseExample({
-//     b: {
-//       c: {
-//         something: { hello: {} }
-//       }
-//     }
-//   })
-//   t.same(a.something.keys(), [], 'a keys are false')
-//   t.same(a.b.something.keys(), [], 'a.b keys are false')
-//   t.same(a.b.c.something.keys(), [ 'hello' ], 'a.b.c keys equal [ "hello" ]')
-//   t.end()
-// })
+  console.log(a.keys(), b.keys())
+*/
