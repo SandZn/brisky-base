25ba25f2ee9a3e5c3678b2ba79a406dae92f6a68
Jim de Beer <jim@vigour.io>
2016-05-28 16:13:28 +0200

    resort

--- test/keys.js
+++ test/keys.js
@@ -83,15 +83,20 @@ test('keys - inheritance', function (t) {
 test('ordered keys', function (t) {
   const base = new Base({
     sort: 'val',
-    d: 4,
-    b: 2,
-    e: 5,
-    a: 1,
-    c: 3
+    d: { val: 4, field: 'ab' },
+    b: { val: 2, field: 'ad' },
+    e: { val: 5, field: 'aa' },
+    a: { val: 1, field: 'ae' },
+    c: { val: 3, field: 'ac' }
   })
   const result = [ 'a', 'b', 'c', 'd', 'e' ]
   result._ = [ 1, 2, 3, 4, 5 ]
   t.same(base.keys(), result, 'correct order')
+
+  const reorder = [ 'e', 'd', 'c', 'b', 'a' ]
+  reorder._ = [ 'aa', 'ab', 'ac', 'ad', 'ae' ]
+  base.set({ sort: 'field' })
+  t.same(base.keys(), reorder, 'resort')
   t.end()
 })
 
