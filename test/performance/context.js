cde2aa1192df53b9580b94e49fa54df3b721af07
Jim de Beer <jim@vigour.io>
2016-05-28 21:19:05 +0200

    context getter optmization

---
+++ test/performance/context.js
@@ -0,0 +1,23 @@
+'use strict'
+const Base = require('../../')
+const test = require('vigour-performance')
+var amount = 1e4
+
+function setKeys () {
+  const a = new Base({ a: 100, b: 100 })
+  const b = new a.Constructor({ key: 'ITS B' })
+  for (let i = 0; i < amount; i++) {
+    b.set({ [i]: 1 })
+  }
+}
+
+function setKeysContext () {
+  const a = new Base({ a: 100, b: 100 })
+  const b = new a.Constructor({ key: 'ITS B' }) //eslint-disable-line
+  for (let i = 0; i < amount; i++) {
+    // much better stratgy is to set it straight on the correct field vs doing weird context getter shit later
+    a.set({ [i]: 1 })
+  }
+}
+
+test(setKeysContext, setKeys, 1, 50)
--- test/performance/keys.js
+++ test/performance/keys.js
@@ -3,19 +3,19 @@ const Base = require('../../')
 const test = require('vigour-performance')
 var amount = 1e4
 
-// function createBase () {
-//   for (let i = 0; i < amount; i++) {
-//     const a = new Base(i) //eslint-disable-line
-//   }
-// }
+function createBase () {
+  for (let i = 0; i < amount; i++) {
+    const a = new Base(i) //eslint-disable-line
+  }
+}
 
-// function createBaseKeys () {
-//   for (let i = 0; i < amount; i++) {
-//     const a = new Base({ a: i }) //eslint-disable-line
-//   }
-// }
+function createBaseKeys () {
+  for (let i = 0; i < amount; i++) {
+    const a = new Base({ a: i }) //eslint-disable-line
+  }
+}
 
-// test(createBaseKeys, createBase, 10)
+test(createBaseKeys, createBase, 10)
 
 function setKeys () {
   const a = new Base({ a: 100, b: 100 })
@@ -25,22 +25,22 @@ function setKeys () {
   }
 }
 
-// test(setKeys, createBaseKeys, 5)
+test(setKeys, createBaseKeys, 5)
 
-// function removeKeys () {
-//   const a = new Base({ a: 100, b: 100 })
-//   const b = new a.Constructor({ key: 'ITS B' })
-//   for (let i = 0; i < amount; i++) {
-//     b.set({ [i]: 1 })
-//   }
-//   for (let i = 0; i < amount; i++) {
-//     // b[i].remove(false, true, true)
-//     b[i].remove()
-//     // b.keys()
-//   }
-// }
+function removeKeys () {
+  const a = new Base({ a: 100, b: 100 })
+  const b = new a.Constructor({ key: 'ITS B' })
+  for (let i = 0; i < amount; i++) {
+    b.set({ [i]: 1 })
+  }
+  for (let i = 0; i < amount; i++) {
+    // b[i].remove(false, true, true)
+    b[i].remove()
+    // b.keys()
+  }
+}
 
-// test(removeKeys, setKeys, 2, 20)
+test(removeKeys, setKeys, 2, 20)
 
 function orderedKeysBest () {
   const b = new Base({
