a8991945b7384f04e6e3eeceeb79af34fae1a28f
Jim de Beer <jim@vigour.io>
2016-05-29 10:16:50 +0200

    clean up context, added tests , removed prev -- lets see if it fails

--- test/context/double.js
+++ test/context/double.js
@@ -9,11 +9,7 @@ test('context - double - override (noContext property)', function (t) {
     nestB: 'nestB',
     noReference: true
   })
-  const c = new Base({
-    cA: {
-      cB: new b.Constructor()
-    }
-  })
+  const c = new Base({ cA: { cB: new b.Constructor() } })
   const d = new c.Constructor()
   t.same(d.cA.cB.nestB.path(), [ 'cA', 'cB', 'nestB' ], 'double context has correct path')
   d.cA.cB.nestB.set('resolve d!')
@@ -26,5 +22,8 @@ test('context - double - override (noContext property)', function (t) {
   t.equal(f.cA.cB.nestB === b.nestB, false, 'resolved double context using remove')
   t.equal(f.cA.cB.nestB, null, 'removed nestB')
   t.same(f.cA.cB.keys(), [], 'remove results in empty keys')
+  const g = new c.Constructor()
+  g.cA.cB.nestB.set('nestB')
+  t.equal(g.cA.cB.nestB === g.nestB, false, 'does resolve if its the same (creates its own)')
   t.end()
 })
