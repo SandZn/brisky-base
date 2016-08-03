76e083bcc9a71b36f897607e3a4c11c22381cf0a
Jim de Beer <jim@vigour.io>
2016-08-03 09:55:49 +0200

    added "root" getter closes #100

---
+++ test/root.js
@@ -0,0 +1,10 @@
+'use strict'
+const test = require('tape')
+const Base = require('../')
+
+test('root', function (t) {
+  const base = new Base({ a: { b: { c: true } } })
+  t.equal(base.a.b.c.getRoot(), base, 'works with a method')
+  t.equal(base.a.b.c.root, base, 'works with getter')
+  t.end()
+})
