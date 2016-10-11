7b0b9da5fe6c4aecfb33acfda1c3da3d9dd40ed9
Jim de Beer <jim@vigour.io>
2016-10-11 17:34:14 +0200

    clean up

--- test/remove.js
+++ test/remove.js
@@ -1,18 +1,18 @@
 'use strict'
 const test = require('tape')
-const Base = require('../base')
+const base = require('../')
 const isRemoved = require('brisky-is-removed')
 
 test('remove', t => {
   t.plan(3)
-  var base = Base({
+  var obj = base({
     a: { b: { c: true } }
   })
-  var c = base.a.b.c
-  base.a.b.c.remove()
+  var c = obj.a.b.c
+  obj.a.b.c.remove()
   t.equal(isRemoved(c), true, 'c is removed')
-  base.a.b.set({ c: true })
-  t.equal(isRemoved(base.a.b.c), false, 'c exists')
-  base.set({ a: null })
-  t.equal(isRemoved(base.a), true, 'base a is removed using set notation')
+  obj.a.b.set({ c: true })
+  t.equal(isRemoved(obj.a.b.c), false, 'c exists')
+  obj.set({ a: null })
+  t.equal(isRemoved(obj.a), true, 'obj a is removed using set notation')
 })
