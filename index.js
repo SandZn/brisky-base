b9da9d878e5fa7106fed0443559441c39c296592
Jim de Beer <jim@vigour.io>
2016-10-12 15:56:50 +0200

    added types

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict'
 const Base = require('./base')
-module.exports = (val, stamp, parent, key, params) =>
-  new Base(val, stamp, parent, key, params)
+module.exports = (val, stamp, parent, key, params) => {
+  return new Base(val, stamp, parent, key, params)
+}
