11e9013edf0c971d0752a31e8b782551da36bc96
Jim de Beer <jim@vigour.io>
2016-10-12 12:36:40 +0200

    more tests

--- test/property/child/complex.js
+++ test/property/child/complex.js
@@ -4,29 +4,57 @@ const base = require('../../..')
 
 test('property - child - complex', t => {
   const a = base({
-    properties: {
-      syncUpIsFn: true,
-      syncDownIsFn: true,
-      syncUp (val) {
-        if (!this.hasOwnProperty('child')) {
-          this.set({ child: { child: 'Constructor' } }, false)
+    types: {
+      a: {
+        properties: {
+          syncUp (val) {
+            this.set({
+              child: {
+                child: 'Constructor'
+              }
+            }, false)
+            this.syncUp = this.child.prototype.syncUp = val
+          },
+          syncDown (val) {
+            this.set({
+              child: {
+                child: 'Constructor'
+              }
+            }, false)
+            this.syncDown = this.child.prototype.syncDown = val
+          },
+          sync (val) {
+            return this.set({
+              syncUp: val,
+              syncDown: val
+            }, false)
+          }
+        },
+        child: 'Constructor'
+      }
+    },
+    child: { type: 'a' }
+  })
+
+  a.set({
+    a: {
+      b: {
+        sync: false,
+        c: {
+          d: true
         }
-        this.child.prototype.syncUp = this.syncUp = val
-      },
-      syncDown (val) {
-        if (!this.hasOwnProperty('child')) {
-          this.set({ child: { child: 'Constructor' } }, false)
-        }
-        this.child.prototype.syncDown = this.syncDown = val
-      },
-      sync (val) {
-        return this.set({
-          syncUp: val,
-          syncDown: val
-        }, false)
+      }
+    },
+    x: {
+      sync: true,
+      y: {
+        sync: false,
+        z: {}
       }
     }
   })
 
-  console.log(a)
+  t.equal(a.x.y.z.syncUp, false, 'nested inherits from first common child construct')
+  t.equal(a.x.syncUp, true, 'does not replace higher up')
+  t.end()
 })
