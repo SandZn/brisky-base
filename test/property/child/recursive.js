fe20f214594f5533ff6c01e0ce35b4f4c91869dd
Jim de Beer <jim@vigour.io>
2016-10-14 14:22:45 +0200

    added recursize types

--- test/property/child/recursive.js
+++ test/property/child/recursive.js
@@ -5,22 +5,48 @@ const base = require('../../../')
 test('property - child', t => {
   const elem = base({
     types: {
-      ul: {
-        tag: 'ul',
-        child: {
-          tag: 'li',
-          type: 'ul' // this -- ultra powerful
-          // would need like this new parent.Constructor()
+      // ul: {
+      //   tag: 'ul',
+      //   child: {
+      //     type: 'ul'
+      //   }
+      // },
+      lulz: {
+        tag: 'lulz',
+        field: {
+          type: 'lulz'
         }
       }
     }
   })
 
+  // console.log('set')
+  // elem.set({ list: { type: 'ul' } })
+  // console.log('set is done')
+  // console.log(elem)
+
+  // elem.set({
+  //   list: {
+  //     field: {
+  //       flups: true
+  //     }
+  //   }
+  // })
+  // console.log(elem)
+
   elem.set({
-    list: { type: 'ul' }
+    lulz: {
+      type: 'lulz',
+      field: {
+        // field: { reset: true, val: 'xxxx' },
+        flups: 'glurrrf'
+      }
+    }
   })
 
-  console.log(elem)
+  console.log(elem.lulz.field.flups)
+
+  console.log(elem.types.lulz.field === elem.lulz.field)
 
   t.end()
 })
